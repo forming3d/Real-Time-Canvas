16bc4b3924e223720b62ce2b922afa0f8957ab97
forming3d <forming3d@gmail.com>
2025-11-10 03:03:12 +0100

    feat: add react-colorful for color picker

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './app.css'; // estilos globales (asegúrate de que existe)
+import './app.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
